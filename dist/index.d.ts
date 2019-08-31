4274c4bb76629fe1eed715a4d682cbc970ba9e2b
Michael <micftk@gmail.com>
2019-08-31 14:07:38 +0800

    fix: fixed type

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,11 @@
-export const map : (resultSet: [], maps: [], mapId: string, columnPrefix: string) => Array<any>
-export const mapOne : (resultSet: [], maps: [], mapId: string, columnPrefix: string) => Object
-export const NotFoundError : () => void
+const map : (resultSet: [], maps: [], mapId: string, columnPrefix: string) => Array<any>
+const mapOne : (resultSet: [], maps: [], mapId: string, columnPrefix: string) => Object
+const NotFoundError : () => void
+
+joinjs = {
+    map,
+    mapOne,
+    NotFoundError
+}
+
+export default joinjs;
