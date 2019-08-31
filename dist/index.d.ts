058f8a602bc7a2257bac9d49b306f2f8f8268828
Michael <micftk@gmail.com>
2019-08-31 14:00:18 +0800

    feat: added type

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export const map : (resultSet: [], maps: [], mapId: string, columnPrefix: string) => Array<any>
+export const mapOne : (resultSet: [], maps: [], mapId: string, columnPrefix: string) => Object
+export const NotFoundError : () => void
